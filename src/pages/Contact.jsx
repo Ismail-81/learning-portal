41bce4aa4dbd3435c8b9c6be5a8872ceb943a217
Ismail Gheewala <ismailgheewala1@gmail.com>
2026-03-14 10:29:32 +0530

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -55,9 +55,7 @@ function Contact() {
       <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Header */}
         <div className="text-center mb-12">
-          <h1 className="text-4xl font-extrabold text-gray-900 tracking-tight mb-4">
-            Get in <span className="text-indigo-600">Touch</span>
-          </h1>
+          
           <p className="text-lg text-gray-600 max-w-2xl mx-auto">
             Have a question, feedback, or just want to say hi? Fill out the form
             below and we'll get back to you as soon as possible.
